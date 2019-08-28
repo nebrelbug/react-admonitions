3c104803a8c8bd5dfb4122f4733e28e7c8e7d0b2
Ben Gubler <nebrelbug@gmail.com>
2019-08-27 21:11:10 -0600

    Hoping this will fix dependency conflicts

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,57 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default [
   {
     input: 'src/index.js',
-    output: {
-      file: 'dist/index.js',
-      format: 'umd',
-      name: 'ReactAdmonitions',
-      sourcemap: true
-    },
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'ReactAdmonitions',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
     plugins: [
       babel({
         exclude: 'node_modules/**'
-      })
-    ]
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true
-    },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**'
-      })
-    ]
+      }),
+      resolve()
+    ],
+    external: ['react', 'react-dom']
   },
   {
     input: 'src/styled.js',
-    output: {
-      file: 'dist/styled.esm.js',
-      format: 'esm',
-      sourcemap: true
-    },
+    output: [
+      {
+        file: 'dist/styled.js',
+        format: 'umd',
+        name: 'ReactAdmonitions',
+        sourcemap: true,
+        globals: {
+          'styled-components': 'styled'
+        }
+      },
+      {
+        file: 'dist/styled.esm.js',
+        format: 'esm',
+        sourcemap: true,
+        globals: {
+          'styled-components': 'styled'
+        }
+      }
+    ],
     plugins: [
       babel({
         exclude: 'node_modules/**'
-      })
+      }),
+      resolve()
     ],
-    external: ['styled-components']
-  },
-  {
-    input: 'src/styled.js',
-    output: {
-      file: 'dist/styled.js',
-      format: 'umd',
-      name: 'ReactAdmonitions',
-      sourcemap: true
-    },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**'
-      })
-    ],
-    external: ['styled-components']
+    external: ['react', 'react-dom', 'styled-components']
   }
 ]
